2a109a131a686bca8d8c625bcf8f0aec947664c9
Jediael Fraser <jedfras@gmail.com>
2020-04-19 18:41:27 -0500

    training and validation

--- prepare_data.py
+++ prepare_data.py
@@ -9,6 +9,7 @@ import string
 import pprint as pp
 import csv
 from typing import List
+import random
 
 class TweetDataGatherer:
 
@@ -41,22 +42,25 @@ class TweetDataGatherer:
     * emoji value: number of emoji in text
     """
 
-    def collect_data(self, filename: str):
-        data = []
+    def collect_data(self):
+        '''
+        Returns a dictionry mapping authors to lists of their tweet data (as described above)
+        '''
+        data_per_author = {}
         for screen_name in self.screen_names:
             statuses = api.GetUserTimeline(screen_name=screen_name, include_rts=False, exclude_replies=True, count=self.count)
             last_id = 0
-            with open(file=filename, mode='a') as csvfile:
-                writer = csv.writer(csvfile)
-                while len(statuses) > 0:
-                    for s in statuses:
-                        text, author = s.full_text, s.user.screen_name
-                        row = [self.prepare_text(text), len(text), self.case_ratio(text), self.punctuation_count(text), author]
-                        writer.writerow(row)
-                        data.append(row)
-                        last_id = s.id
-                    statuses = api.GetUserTimeline(screen_name=screen_name, include_rts=False, exclude_replies=True, count=self.count, max_id=last_id - 1)
-        return data
+            while len(statuses) > 0:
+                for s in statuses:
+                    text, author = s.full_text, s.user.screen_name.lower()
+                    row = [self.prepare_text(text), len(text), self.case_ratio(text), self.punctuation_count(text)]
+                    if author in data_per_author:
+                        data_per_author[author].append(row)
+                    else:
+                        data_per_author[author] = [row]
+                    last_id = s.id
+                statuses = api.GetUserTimeline(screen_name=screen_name, include_rts=False, exclude_replies=True, count=self.count, max_id=last_id - 1)
+        return data_per_author
 
 # Initialize Twitter api
 api = twitter.Api(consumer_key=ss.CONSUMER_KEY,
@@ -65,12 +69,41 @@ api = twitter.Api(consumer_key=ss.CONSUMER_KEY,
     access_token_secret=ss.SECRET,
     tweet_mode='extended')
 
-# Overrwite current file first
-filename = 'tweet_data.csv'
-with open(file=filename, mode='w') as f:
-    f.write('')
+# Get all tweets from each user
+gatherer = TweetDataGatherer(api=api, screen_names=['thetalkingjed', 'isnascarasport', 'wildenian_thot'])
+all_per_user = gatherer.collect_data()
 
-# Gather data
-gatherer = TweetDataGatherer(api=api, screen_names=['thetalkingjed', 'isnascarasport', 'Wildenian_Thot'])
-data = gatherer.collect_data(filename)
-pp.pprint(data)
+# Randomly sample 250 from each user
+training_per_user = {}
+training_list = []
+validation_list = []
+sample_size = 250
+for key in all_per_user.keys():
+    data = all_per_user[key]
+    training_per_user[key] = random.sample(data, sample_size)
+
+# Randomly sample validation data (50 from each user) and build lists
+for key in training_per_user.keys():
+    data = training_per_user[key]
+    random.shuffle(data)
+    removed = [data.pop() for _ in range(50)]
+    training_list.extend([(entry + [key]) for entry in data])
+    validation_list.extend([(entry + [key]) for entry in removed])
+
+# Shuffle all training and validation data
+random.shuffle(training_list)
+random.shuffle(validation_list)
+
+# Write to .csv files
+training_file = 'training.csv'
+validation_file = 'validation.csv'
+
+with open(training_file, mode='w') as csvfile:
+    writer = csv.writer(csvfile)
+    for data in training_list:
+        writer.writerow(data)
+
+with open(validation_file, mode='w') as csvfile:
+    writer = csv.writer(csvfile)
+    for data in validation_list:
+        writer.writerow(data)
